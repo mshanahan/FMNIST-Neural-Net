33e7088f3ecd8c6cff1674533d3d5811adf8dc0e
Michael Shanahan <mshanahan@huskers.unl.edu>
2018-02-06 22:56:46 -0600

    typo

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def main(argv):
   labels = tf.placeholder(tf.float32, [None, 10], name='labels')
   cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=labels, logits=my_network)
   confusion_matrix_op = tf.confusion_matrix(tf.argmax(labels, axis=1), tf.argmax(my_network, axis=1), num_classes=10)
-  REG_COEF = 0.0001
+  REG_COEFF = 0.0001
   regularization_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
   total_loss = cross_entropy + REG_COEFF * sum(regularization_losses)
 
