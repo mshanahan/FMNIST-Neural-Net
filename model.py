daabb22a9687e62f4dd314f25fce423e873654e8
Michael Shanahan <michael.shanahan5@gmail.com>
2018-02-07 17:31:45 -0600

    normalized inputs

--- model.py
+++ model.py
@@ -7,12 +7,16 @@ def my_model(layer_counts, inputs):
   #layer_counts: a 4d array of integers
   #[0]: hidden 1 size, [1]: hidden 2 size
   with tf.name_scope('my_model') as scope:
+	divided_input = inputs / 255
+  
     hidden_1 = tf.layers.dense(
-      inputs,
+      divided_inputs,
       layer_counts[0],
       activation = tf.nn.relu,
       bias_regularizer = tf.contrib.layers.l2_regularizer(1.0),
+	  bias_initializer=tf.zeros_initializer(),
       kernel_regularizer = tf.contrib.layers.l2_regularizer(1.0),
+	  kernel_initializer=None,
       name = 'hidden_1')
       
     hidden_2 = tf.layers.dense(
@@ -20,15 +24,17 @@ def my_model(layer_counts, inputs):
       layer_counts[1],
       activation = tf.nn.relu,
       bias_regularizer = tf.contrib.layers.l2_regularizer(1.0),
+	  bias_initializer=tf.zeros_initializer(),
       kernel_regularizer = tf.contrib.layers.l2_regularizer(1.0),
+	  kernel_initializer=None,
       name = 'hidden_2')
       
     output = tf.layers.dense(
       hidden_2,
       10,
       activation = tf.nn.relu,
-      bias_regularizer = tf.contrib.layers.l2_regularizer(1.0),
-      kernel_regularizer = tf.contrib.layers.l2_regularizer(1.0),
+	  bias_initializer=tf.zeros_initializer(),
+	  kernel_initializer=None,
       name = 'output')
       
     return output
