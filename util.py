84f1625c8278d4573ee46cc5e56062a7fd13093f
Michael Shanahan <mshanahan@huskers.unl.edu>
2018-02-08 09:37:22 -0600

    added classification rate

--- util.py
+++ util.py
@@ -10,6 +10,7 @@ def split_data(data, labels, proportion):
     s = np.random.permutation(size)
     split_idx = int(proportion * size)
     return data[s[:split_idx]], data[s[split_idx:]], labels[s[:split_idx]], labels[s[split_idx:]]
+#converts the given labels into white-hot vectors
 def white_hot(labels):
   label_shape = ( len(labels), 10 ) #n rows, 10 columns
   new_labels = np.zeros(label_shape)
@@ -18,8 +19,10 @@ def white_hot(labels):
     new_labels[int(idx),int(i)] = 1
     idx += 1
   return new_labels
-#  shape = 
-#  label_shape = ( len(fmnist_labels), 10 ) #n rows, 10 columns
-#  new_array = np.zeros(label_shape)
-#  new_array[np.arange(len(labels)),labels] = 1
-#  return new_array
+#given a confusion matrix and a dimensionality, calculates the proportion of correct answers
+def classification_rate(conf_matrix,dim):
+  sum_entries = sum(sum(conf_matrix)
+  sum_correct = 0
+  for i in range(0,dim):
+    sum_correct += conf_matrix[i,i]
+  return float(sum_correct / sum_entries)
