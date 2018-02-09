0511a54b041d84876de191d3d96f96bfb0293235
Michael Shanahan <mshanahan@huskers.unl.edu>
2018-02-08 22:57:26 -0600

    added model scope

--- model.py
+++ model.py
@@ -9,41 +9,40 @@ def my_model(layer_counts, inputs):
 
   #layer_counts: a 4d array of integers
   #[0]: hidden 1 size, [1]: hidden 2 size
-  with tf.name_scope('my_model') as scope:
-    divided_inputs = inputs / 255
-  
-    hidden_1 = tf.layers.dense(
-      divided_inputs,
-      layer_counts[0],
-      activation = tf.nn.relu,
-      bias_regularizer = regularizer,
-      kernel_regularizer = regularizer,
-      kernel_initializer = initializer,
-      name = 'hidden_1')
-      
-    hidden_2 = tf.layers.dense(
-      hidden_1,
-      layer_counts[1],
-      activation = tf.nn.relu,
-      bias_regularizer = regularizer,
-      kernel_regularizer = regularizer,
-      kernel_initializer = initializer,
-      name = 'hidden_2')
+  divided_inputs = inputs / 255
 
-    hidden_3 = tf.layers.dense(
-      hidden_2,
-      layer_counts[2],
-      activation = tf.nn.relu,
-      bias_regularizer = regularizer,
-      kernel_regularizer = regularizer,
-      kernel_initializer = initializer,
-      name = 'hidden_3')
-      
-    output = tf.layers.dense(
-      hidden_3,
-      10,
-      activation = tf.nn.relu,
-      kernel_initializer = initializer,
-      name = 'output')
-      
-    return output
+  hidden_1 = tf.layers.dense(
+    divided_inputs,
+    layer_counts[0],
+    activation = tf.nn.relu,
+    bias_regularizer = regularizer,
+    kernel_regularizer = regularizer,
+    kernel_initializer = initializer,
+    name = 'hidden_1')
+
+  hidden_2 = tf.layers.dense(
+    hidden_1,
+    layer_counts[1],
+    activation = tf.nn.relu,
+    bias_regularizer = regularizer,
+    kernel_regularizer = regularizer,
+    kernel_initializer = initializer,
+    name = 'hidden_2')
+
+  hidden_3 = tf.layers.dense(
+    hidden_2,
+    layer_counts[2],
+    activation = tf.nn.relu,
+    bias_regularizer = regularizer,
+    kernel_regularizer = regularizer,
+    kernel_initializer = initializer,
+    name = 'hidden_3')
+
+  output = tf.layers.dense(
+    hidden_3,
+    10,
+    activation = tf.nn.relu,
+    kernel_initializer = initializer,
+    name = 'output')
+
+  return output
